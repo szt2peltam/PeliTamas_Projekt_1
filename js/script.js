9119dd283005817035caf88e9f12fa69eb1a87ff
unknown <pelitamas494@gmail.com>
2023-09-23 12:40:40 +0200

    asd

--- js/script.js
+++ js/script.js
@@ -50,33 +50,58 @@ function AddNewToDo(dayIndex){
     ToDoCount++;
 }
 
-function CloseModal(){
+function CloseModal(element, listener){
     Modal.classList.remove("visible");
     Modal.classList.add("not-visible");
+    element.removeEventListener("click",listener)
 }
+
 
 function OpenModal(){
     Modal.classList.add("visible");
     Modal.classList.remove("not-visible");
-
+    colorInput = 0;
+    Colors.forEach(element => {
+        element.classList.remove("isSelected")
+    });
 }
-const SaveButton = document.querySelector(".save");
+let SaveButton = document.querySelector(".save");
 function ToDoClicked(ToDoNumber){
 
     OpenModal();
     SaveButton.addEventListener("click", function EventListener(){
         SaveData(ToDoNumber)
         this.removeEventListener("click", EventListener);
-        CloseModal();
+        CloseModal(this, EventListener);
+        DeleteBtn.replaceWith(DeleteBtn.cloneNode(true))
+        DeleteBtn = document.querySelector(".delete")
     })
 
-    DeleteBtn.addEventListener("click", function DeleteElement() {
+    DeleteBtn.addEventListener("click", function EventListener() {
         Delete(ToDoNumber);
-        this.removeEventListener("click", DeleteElement);
-        CloseModal();
+        this.removeEventListener("click", EventListener);
+        CloseModal(this,EventListener);
+        SaveButton.replaceWith(SaveButton.cloneNode(true))
+        SaveButton = document.querySelector(".save")
+
     })
 }
+let colorInput = 0;
+const Colors = document.querySelectorAll(".color")
 
+Colors.forEach(element => {
+    element.addEventListener("click", (e) =>{
+        colorInput = e.currentTarget.dataset.color;
+        Colors.forEach(element => {
+            if(element.dataset.color == e.currentTarget.dataset.color){
+                element.classList.add("isSelected");
+            }else{
+                element.classList.remove("isSelected");
+            }
+            
+        });
+    })
+});
 const FromInput = document.querySelector(".from");
 const ToInput = document.querySelector(".to");
 const LessonInput = document.querySelector(".lesson");
@@ -86,11 +111,13 @@ function SaveData(ToDoNumber){
     if(FromInput.value == "") return;
     if(ToInput.value == "") return;
     if(LessonInput.value == "") return;
-
+    if(colorInput == 0) return;
 
     ListOfTodoElements.forEach(element => {
         if(element.dataset.todocount == ToDoNumber){
-            console.log(element.childNodes);
+            element.className = "";
+            element.classList.add("todo")
+            element.classList.add(`box-shadow-${colorInput}`)
             element.childNodes[1].innerText = FromInput.value;
             element.childNodes[5].innerText = ToInput.value;
             element.childNodes[3].innerText = LessonInput.value;
@@ -101,7 +128,7 @@ function SaveData(ToDoNumber){
 
 
 
-const DeleteBtn = document.querySelector(".delete")
+let DeleteBtn = document.querySelector(".delete")
 
 
 function Delete(ToDoNumber) {
